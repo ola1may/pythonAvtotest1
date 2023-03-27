758854f103b0df29666fe83fea6e27e77ee64ae7
ola1may <108890527+ola1may@users.noreply.github.com>
2023-03-27 17:32:24 +0300

    Update wildberris_find.py

--- wildberris_find.py
+++ wildberris_find.py
@@ -1,22 +1,22 @@
 from selenium import webdriver
+import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-find_item = ['косметика','мебель','джинсы']
 link = 'https://www.wildberries.ru/'
 browser = webdriver.Chrome()
 browser.maximize_window()
 browser.get(link)
+time.sleep(2)
 search_str = browser.find_element(By.ID, 'searchInput')
-
-for el in find_item:
-    search_str.clear()
-    search_str.send_keys(el)
-    search_str.send_keys(Keys.ENTER)
-    detect = WebDriverWait(browser, 10).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, ".dropdown-filter__btn.dropdown-filter__btn--burger")))
-    text = detect.text
-    if text==el:
-        print('ok')
+el ='косметика'
+search_str.clear()
+search_str.send_keys(el)
+search_str.send_keys(Keys.ENTER)
+detect=WebDriverWait (browser, 10).until (
+    EC.visibility_of_element_located ((By.CSS_SELECTOR, ".dropdown-filter__btn.dropdown-filter__btn--burger")))
+text=detect.text
+if text == el:
+    print ('ok')
