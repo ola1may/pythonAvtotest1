8e0e20391667dbd64ce43a3345c7c51cf630f577
ola1may <108890527+ola1may@users.noreply.github.com>
2023-03-29 11:35:23 +0300

    Update wildberris_find.py

--- wildberris_find.py
+++ wildberris_find.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
-find_item = ['косметика','мебель','джинсы']
+find_item = ['косметика','мебель','123']
 link = 'https://www.wildberries.ru/'
 browser = webdriver.Chrome()
 browser.maximize_window()
